97361a48426ddf18e1ccdaec952628fc52d3df67
Sheetal Bajaj <sheetalbajajgodda@gmail.com>
2024-11-27 11:39:56 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 using namespace std;
+int ap(int n)
+{
+   int out=((3*n)+7);
+   return out;
+}
 
 int main()
 {
-    for(int i=0;i<=5;i++)
-    {
-        cout<<i<<" ";
-        i++;
-    }
+    int n;
+    cout<<"Enter the term you want to find:"<<endl;
+    cin>>n;
+    int ans=ap(n);
+    cout<<"nth term= "<<ans<<endl;
 }

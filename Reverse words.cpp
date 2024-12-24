fff1f9e571bd4af43e1eace3f28bee6b08c4a1b2
Sheetal Bajaj <sheetalbajajgodda@gmail.com>
2024-12-25 00:38:57 +0530

    Reverses letters of word in string

---
+++ Reverse words.cpp
@@ -0,0 +1,26 @@
+//Reverse words
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string s="Hello World!";
+    int start=0;
+    int p=0;
+    for(int i=0;i<=s.length();i++)
+    {
+        if(s[p]==' '||s[p]=='\0')
+        {
+            int end=p-1;
+            while(start<=end)
+            {
+                swap(s[start],s[end]);
+                start++;
+                end--;
+            }
+            start=p+1;
+        }
+        p++;
+    }
+     cout<<s;
+}

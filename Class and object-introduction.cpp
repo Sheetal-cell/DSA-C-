db519611cd9ae028c6d4fef149d96a3df9589fef
Sheetal Bajaj <sheetalbajajgodda@gmail.com>
2024-12-31 13:22:06 +0530

    Class and object introduction

---
+++ Class and object-introduction.cpp
@@ -0,0 +1,15 @@
+//Class and object introduction
+#include<iostream>
+using namespace std;
+
+class Hero{
+ //Properties
+ int health;
+ };
+
+ int main()
+ {
+     //creation of object
+     Hero h1;
+     cout<<"Size = "<<sizeof(h1);
+ }
